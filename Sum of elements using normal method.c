eb925e1af35d19d4feb4f5f4ee6f5eabe60f6baa
Drushya21KB <154456589+Drushya21KB@users.noreply.github.com>
2024-07-10 11:48:56 +0530

    Add files via upload

---
+++ Sum of elements using normal method.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Function to find the sum of elements in an array (Normal method)
+int sumNormal(int arr[], int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int result = sumNormal(arr, n);
+    printf("Sum of elements (Normal method): %d\n", result);
+
+    return 0;
+}
